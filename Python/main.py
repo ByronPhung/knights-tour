46890e4b1e2f96143b983aeea70febb4ffd626c1
Byron Phung <byronphung@gmail.com>
2016-04-14 16:09:00 -0700

    Fixed missing code

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,3 @@
-import sys
 from knight import Knight
 from board import Board
 
@@ -95,13 +94,17 @@ y = 9
 
 # Take x & y initial inputs. Loop while invalid.
 while (x < 1 or x > BOARD_SIZE) or (y < 1 or y > BOARD_SIZE):
+    x, y = input("Enter x & y (1-" + str(BOARD_SIZE)
+                 + ") separated by a space (e.g. 4 4): ").split()
     x, y = [int(x), int(y)]
 
-    if ((x < 0) or (x > BOARD_SIZE - 1)):
-        print("ERROR:: x must be between 0 and " + str(BOARD_SIZE - 1))
+    if x < 1 or x > BOARD_SIZE:
+        print("ERROR:: x must be 1-" + str(BOARD_SIZE))
 
-    if ((y < 0) or (y > BOARD_SIZE - 1)):
-        print("ERROR:: y must be between 0 and " + str(BOARD_SIZE - 1))
+    if y < 1 or y > BOARD_SIZE:
+        print("ERROR:: y must be 1-" + str(BOARD_SIZE))
+
+print()
 
 # Create a Knight object to move around the board.
 knight = Knight(x, y)
@@ -110,22 +113,28 @@ knight = Knight(x, y)
 board = Board(BOARD_SIZE)
 board.place_knight(1, knight.x, knight.y)
 
-for i in range(2, MAX_MOVES + 1):
-    num_possibilities = get_num_possibilities(knight, board)
+# Test special cases for all the moves.
+for current_move in range(2, MAX_MOVES + 1):
+    num_possibilities, next_x, next_y = get_num_possibilities(knight, board)
+    min_exits_idx = 0
+
+    # If there are no possibilities left, then end the tour prematurely.
+    if num_possibilities == 0:
+        print("The knight's tour ended prematurely at (" + str(knight.x + 1)
+              + "," + str(knight.y + 1) + ") during move #"
+              + str(current_move - 1) + ".")
+        print()
+        break
+    
+    # If there is more than 1 possibility, then find the next squares with the
+    # minimum number of exits.
+    elif num_possibilities > 1:
+        exits = find_min_exits(board, num_possibilities, next_x, next_y)
+        min_exits_idx = get_idx_smallest_num_exits(num_possibilities, exits)
+
+    # Move the knight and mark its position on the board.     
+    knight.move(next_x[min_exits_idx], next_y[min_exits_idx])
+    board.place_knight(current_move, knight.x, knight.y)
 
 # Print out the board.
 board.print_board()
-
-def get_num_possibilities(knight, board):
-    """Test each of the eight squares one knight's move away from (I,J) and
-    form a list of the possibilities for the next square (next_i(l), next_j(l)).
-
-    Keyword arguments:
-    <None>
-    """
-    num_possibilities = 0
-    
-
-        if ((temp_x >= 0) and (temp_x < BOARD_SIZE)
-            and (temp_y >= 0) and (temp_y < BOARD_SIZE)
-            and board.is_empty(temp_x, temp_y)):
